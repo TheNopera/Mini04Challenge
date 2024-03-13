e5331e75540cea610cd891216f750b597b4cdbb0
Daniel Ishida <ishidadecol@gmail.com>
2024-03-13 14:46:51 -0300

    Project base added

---
+++ MiniChallenge04/MiniChallenge04/MiniChallenge04App.swift
@@ -0,0 +1,17 @@
+//
+//  MiniChallenge04App.swift
+//  MiniChallenge04
+//
+//  Created by Daniel Ishida on 13/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MiniChallenge04App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
