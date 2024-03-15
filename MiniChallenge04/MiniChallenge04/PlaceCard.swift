9f65ec70ce5db383f1da81eca867ca87a72c8583
Daniel Ishida <ishidadecol@gmail.com>
2024-03-15 14:37:08 -0300

    Place card template created
    Criei o o template de card para testar a chamada de imagem da API

--- MiniChallenge04/MiniChallenge04/PlaceCard.swift
+++ MiniChallenge04/MiniChallenge04/PlaceCard.swift
@@ -12,17 +12,37 @@ struct CardModifier : ViewModifier{
     var cornerRadius : CGFloat
     var title : String
     var image : Image
+    var width : CGFloat = 130
+    var height : CGFloat = 210
     func body(content: Content) -> some View {
-        
+        content
+            .overlay(
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .clipShape(RoundedRectangle(cornerRadius: 25))
+            )
+            .overlay(
+                VStack{
+                    Text(title)
+                        .bold()
+                        .foregroundStyle(.white)
+                        .padding(5)
+                    Spacer()
+                }
+                
+            )
+            .frame(width: width,height: height)
     }
 }
 
 
 struct CardView : View {
     var body: some View {
-        RoundedRectangle(cornerRadius: 25)
-            .modifier(CardModifier(cornerRadius: 25, title: "Rio de Janeiro", image: Image(systemName: "")))
-        
+        VStack{
+            RoundedRectangle(cornerRadius: 25)
+                .modifier(CardModifier(cornerRadius: 25, title: "Rio de Janeiro", image: Image("Cristo")))
+        }
     }
 }
 
