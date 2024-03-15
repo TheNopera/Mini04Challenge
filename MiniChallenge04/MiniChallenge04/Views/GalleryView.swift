c6bbf972d58ee4a609b0a9541091496e37e1dd89
zicaacademy <138627459+zicaacademy@users.noreply.github.com>
2024-03-15 14:36:08 -0300

    Pastas/arquitetura

---
+++ MiniChallenge04/MiniChallenge04/Views/GalleryView.swift
@@ -0,0 +1,18 @@
+//
+//  GalleryView.swift
+//  MiniChallenge04
+//
+//  Created by Luiz Felipe on 15/03/24.
+//
+
+import SwiftUI
+
+struct GalleryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GalleryView()
+}
---
+++ MiniChallenge04/MiniChallenge04/Views/OmboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OmboardingView.swift
+//  MiniChallenge04
+//
+//  Created by Luiz Felipe on 15/03/24.
+//
+
+import SwiftUI
+
+struct OmboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OmboardingView()
+}
