cd2232746ff17402e937d4c52162a646c368b41a
Lukezin03 <117870645+Lukezin03@users.noreply.github.com>
2024-03-22 17:54:11 -0300

    Adicionando o dicionario de UFs

--- MiniChallenge04/MiniChallenge04/Views/MapView.swift
+++ MiniChallenge04/MiniChallenge04/Views/MapView.swift
@@ -10,6 +10,8 @@ import Photos
 
 struct MapView: View {
     
+    let ufs = ["AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"]
+    
     var body: some View {
         
         
