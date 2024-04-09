767e1ea469942d1d836e0ccc792102aca2f04843
zicaacademy <138627459+zicaacademy@users.noreply.github.com>
2024-04-09 15:32:54 -0300

    aaaa

--- MiniChallenge04/MiniChallenge04/Models/RecomendationModel.swift
+++ MiniChallenge04/MiniChallenge04/Models/RecomendationModel.swift
@@ -38,72 +38,55 @@ enum UF:String{
 }
 
 class RecomendationModel {
-    var recomendationCamping:[UF:String] = [
-        .SP:"Morro do Saboó – São Roque",
-        .SP:"Pico do Jaraguá – Serra da Cantareira",
-        .RJ:"Praia do Sono – Paraty",
-        .RJ:"Morro do Couto – Resende",
-        .ES:"Pedra do Camelo – Pancas",
-        .MG:"Complexo do Tira Prosa – Carrancas",
-        .MG:"Paraíso Perdido – Capitólio",
-        .SC:"Praia do Ferrugem – Garopaba",
-        .SC:"Lagoinha do Leste – Florianópolis",
-        .PR:"Ilha do Mel – Baía de Paranaguá",
-        .RS:"Pico do Monte Negro – São José dos Ausentes",
-        .MS:"Serra da Bodoquena – Pantanal",
-        .GO:"Chapada dos Veadeiros",
-        .BA:"Cumuruxatiba, Prado",
-        .BA:"Reserva da Sapiranga – Mata de São João",
-        .AM:"Serra do Aracá – Barcelos",
-        .PA:"Floresta Nacional dos Tapajós – Belterra"
+    var recomendationCamping: [UF: [String]] = [
+        .SP: ["Morro do Saboó – São Roque", "Pico do Jaraguá – Serra da Cantareira"],
+        .RJ: ["Praia do Sono – Paraty", "Morro do Couto – Resende"],
+        .ES: ["Pedra do Camelo – Pancas"],
+        .MG: ["Complexo do Tira Prosa – Carrancas", "Paraíso Perdido – Capitólio"],
+        .SC: ["Praia do Ferrugem – Garopaba", "Lagoinha do Leste – Florianópolis"],
+        .PR: ["Ilha do Mel – Baía de Paranaguá"],
+        .RS: ["Pico do Monte Negro – São José dos Ausentes"],
+        .MS: ["Serra da Bodoquena – Pantanal"],
+        .GO: ["Chapada dos Veadeiros"],
+        .BA: ["Cumuruxatiba, Prado", "Reserva da Sapiranga – Mata de São João"],
+        .AM: ["Serra do Aracá – Barcelos"],
+        .PA: ["Floresta Nacional dos Tapajós – Belterra"]
     ]
-    
-    var recomendationBeach:[UF:String] = [
-        .AL:"Praia do Gunga – Roteiro",
-        .BA:"Praia do Espelho – Trancoso",
-        .SP:"Praia do Félix – Ubatuba",
-        .RJ:"Prainhas – Arraial do Cabo",
-        .RJ:"Praia do Aventureiro – Ilha Grande",
-        .AL:"Praia de Antunes – Maragogi",
-        .PE:"Praia dos Carneiros – Tamandaré",
-        .PB:"Praia de Tambaba – Conde",
-        .RN:"Praia do Madeiro – Pipa",
-        .PI:"Barra Grande – Cajueiro da Praia",
-        .CE:"Praia de Jericoacoara – Jericoacoara",
-        .BA:"Praia de Jeribucaçu – Itacaré",
-        .SP:"Praia do Bonete – Ilhabela",
-        .PA:"Ilha do Amor – Alter do Chão",
-        .AM:"Praia do Tupé – Manaus",
-        .SC:"Ilha do Campeche – Florianópolis",
-        .PE:"Porto de Galinhas – Ipojuca",
-        .BA:"Taipu de Fora – Península de Maraú",
-        .SC:"Guarda do Embaú – Palhoça",
-        .RN:"Parrachos de Maracajaú – Maracajaú",
-        .CE:"Morro Branco – Beberibe"
+
+    var recomendationBeach: [UF: [String]] = [
+        .AL: ["Praia do Gunga – Roteiro", "Praia de Antunes – Maragogi"],
+        .BA: ["Praia do Espelho – Trancoso", "Praia de Jeribucaçu – Itacaré", "Ilha do Amor – Alter do Chão", "Taipu de Fora – Península de Maraú"],
+        .SP: ["Praia do Félix – Ubatuba", "Praia do Bonete – Ilhabela"],
+        .RJ: ["Prainhas – Arraial do Cabo", "Praia do Aventureiro – Ilha Grande"],
+        .PE: ["Praia dos Carneiros – Tamandaré", "Porto de Galinhas – Ipojuca"],
+        .PB: ["Praia de Tambaba – Conde"],
+        .RN: ["Praia do Madeiro – Pipa", "Parrachos de Maracajaú – Maracajaú"],
+        .PI: ["Barra Grande – Cajueiro da Praia"],
+        .CE: ["Praia de Jericoacoara – Jericoacoara", "Morro Branco – Beberibe"],
+        .SC: ["Ilha do Campeche – Florianópolis", "Guarda do Embaú – Palhoça"],
+        .PA: ["Ilha do Amor – Alter do Chão"],
+        .AM: ["Praia do Tupé – Manaus"]
     ]
-    
-    var recomendationForest:[UF:String] = [
-        .RJ:"Parque Nacional da Tijuca - Rio de Janeiro",
-        .SP:"Parque Estadual da Cantareira - Núcleo Pedra Grande",
-        .PA:"Floresta Nacional dos Tapajós",
-        .AM:"Parque Nacional Anavilhanas - Manaus",
-        .MT:"Chapada dos Guimarães",
-        .MS:"Bonito",
-        .AM:"Parque Nacional Jaú",
-        .MT:"Parque Florestal de Sinop"
+
+    var recomendationForest: [UF: [String]] = [
+        .RJ: ["Parque Nacional da Tijuca - Rio de Janeiro"],
+        .SP: ["Parque Estadual da Cantareira - Núcleo Pedra Grande"],
+        .PA: ["Floresta Nacional dos Tapajós"],
+        .AM: ["Parque Nacional Anavilhanas - Manaus", "Parque Nacional Jaú"],
+        .MT: ["Chapada dos Guimarães", "Parque Florestal de Sinop"],
+        .MS: ["Bonito"]
     ]
-    
-    var recomendationMontain:[UF:String] = [
-        .RJ:"Dedo de Deus, Serra dos Órgãos",
-        .SP:"Pedra do Baú, São Bento do Sapucaí",
-        .RR:"Monte Roraima, Serra de Pacaraíma",
-        .MG:"Pedra Riscada, São José do Divino",
-        .PR:"Pico Paraná, Serra do Mar",
-        .ES:"Pedra Azul, Domingos Martins",
-        .RJ:"Agulhas Negras, Itatiaia",
-        .MG:"Pedra da Mina, Serra da Mantiqueira ",
-        .BA:"Morro do Pai Inácio, Chapada Diamantina "
+
+    var recomendationMontain: [UF: [String]] = [
+        .RJ: ["Dedo de Deus, Serra dos Órgãos", "Agulhas Negras, Itatiaia"],
+        .SP: ["Pedra do Baú, São Bento do Sapucaí"],
+        .RR: ["Monte Roraima, Serra de Pacaraíma"],
+        .MG: ["Pedra Riscada, São José do Divino", "Pedra da Mina, Serra da Mantiqueira"],
+        .PR: ["Pico Paraná, Serra do Mar"],
+        .ES: ["Pedra Azul, Domingos Martins"],
+        .BA: ["Morro do Pai Inácio, Chapada Diamantina"]
     ]
+
     
 
 
